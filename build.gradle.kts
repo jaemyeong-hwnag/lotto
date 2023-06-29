5384460ade59e05d51ea51901e00b89227505774
jaemyeong-hwnag <woaudsla@gmail.com>
2023-06-29 23:55:12 +0900

    .gitignore local 설정 추가, db dependencies 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,14 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // log
+    implementation("org.springframework.boot:spring-boot-starter-log4j2")
+
+    // db
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("com.mysql:mysql-connector-j")
 }
 
 tasks.withType<KotlinCompile> {
