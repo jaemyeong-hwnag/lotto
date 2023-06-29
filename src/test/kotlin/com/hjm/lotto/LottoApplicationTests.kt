e2c67b9c499e67eb33b7cd218a0ce4ebf8056dc2
jaemyeong-hwnag <woaudsla@gmail.com>
2023-06-29 23:36:48 +0900

    최초 설정

---
+++ src/test/kotlin/com/hjm/lotto/LottoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hjm.lotto
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LottoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
